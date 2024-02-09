08a6670d42b32275a10b4727bd3a39683a40211f
Mangesh Landge <mangeshbkwsu@gmail.com>
2024-02-09 12:40:07 +0530

    code refactoring

--- scripts/runBot.js
+++ scripts/runBot.js
@@ -13,6 +13,73 @@ const MaxValue =
   "57896044618658097711785492504343953926634992332820282019728792003956564819967";
 const USER_ADDRESS = "0x4EB491B0fF2AB97B9bB1488F5A1Ce5e2Cab8d601";
 
+async function liquidatePosition(
+  position,
+  FlashLiquidate,
+  accounts,
+  helperContract
+) {
+  try {
+    const isToken0 = position.liquidableToken == "token0";
+    const isStableCoin = position[position.liquidableToken].decimals === 6;
+
+    let payload = [
+      isToken0 ? position.token0.id : position.token1.id,
+      3000,
+      new BigNumber(
+        isToken0 ? position.borrowBalance0 : position.borrowBalance1
+      )
+        .plus(isStableCoin ? 10 ** 2 : 10 ** 12)
+        .toFixed(),
+      position.pool,
+      position.owner,
+      isToken0 ? position.token1.id : position.token0.id,
+      isToken0 ? `-${MaxValue}` : MaxValue,
+    ];
+
+    console.log(
+      `--------------started Liquidation for position ${position.id}------------------`
+    );
+    console.log("PAYLOAD: ", payload);
+    const flash = await FlashLiquidate.initFlash(payload);
+    const receipt = await flash.wait();
+    const txHash = receipt.hash;
+    const txData = {
+      positionID: position.id,
+      transactionHash: txHash,
+      timestamp: new Date().toLocaleString(),
+      user: accounts[0],
+      positionOwner: position.owner,
+    };
+    logger.info(JSON.stringify(txData));
+
+    console.log(
+      `--------------completed Liquidation for position ${position.id}------------------`
+    );
+
+    const userData = await helperContract.getPoolFullData(
+      "0x6D922876074cCA3ef3fB16D63dc45D72D9C4F2A0",
+      position.pool,
+      position.owner
+    );
+
+    console.log(
+      `after liquidation of position ${position.id}`,
+      `borrowBalance: ${hre.ethers.formatEther(
+        isToken0 ? userData._borrowBalance0 : userData._borrowBalance1
+      )}`,
+      `lendBalance: ${hre.ethers.formatEther(
+        isToken0 ? userData._lendBalance1 : userData._lendBalance0
+      )}`,
+      `_healthFactor0: ${userData._healthFactor0}`,
+      `_healthFactor1: ${userData._healthFactor1}`
+    );
+  } catch (error) {
+    console.error(`An error occurred in Position ${position.id}: `, error);
+    logger.error(`An error occurred in Position ${position.id}: `, error);
+  }
+}
+
 async function main() {
   try {
     const FlashLiquidate = await hre.ethers.deployContract("FlashLiquidate", [
@@ -48,72 +115,19 @@ async function main() {
       helperContract
     );
 
-    const liquidatePosition = async (position) => {
-      try {
-        const isToken0 = position.liquidableToken == "token0";
-        const isStableCoin = position[position.liquidableToken].decimals === 6;
-
-        let payload = [
-          isToken0 ? position.token0.id : position.token1.id,
-          3000,
-          new BigNumber(
-            isToken0 ? position.borrowBalance0 : position.borrowBalance1
-          )
-            .plus(isStableCoin ? 10 ** 2 : 10 ** 12)
-            .toFixed(),
-          position.pool,
-          position.owner,
-          isToken0 ? position.token1.id : position.token0.id,
-          isToken0 ? `-${MaxValue}` : MaxValue,
-        ];
-
-        console.log(
-          `--------------started Liquidation for position ${position.id}------------------`
-        );
-        console.log("PAYLOAD: ", payload);
-        const flash = await FlashLiquidate.initFlash(payload);
-        const receipt = await flash.wait();
-        const txHash = receipt.hash;
-        const txData = {
-          positionID: position.id,
-          transactionHash: txHash,
-          timestamp: new Date().toLocaleString(),
-          user: accounts[0],
-          positionOwner: position.owner,
-        };
-        logger.info(JSON.stringify(txData));
-
-        console.log(
-          `--------------completed Liquidation for position ${position.id}------------------`
-        );
-
-        const userData = await helperContract.getPoolFullData(
-          "0x6D922876074cCA3ef3fB16D63dc45D72D9C4F2A0",
-          position.pool,
-          position.owner
-        );
-
-        console.log(
-          `after liquidation of position ${position.id}`,
-          `borrowBalance: ${hre.ethers.formatEther(
-            isToken0 ? userData._borrowBalance0 : userData._borrowBalance1
-          )}`,
-          `lendBalance: ${hre.ethers.formatEther(
-            isToken0 ? userData._lendBalance1 : userData._lendBalance0
-          )}`,
-          `_healthFactor0: ${userData._healthFactor0}`,
-          `_healthFactor1: ${userData._healthFactor1}`
-        );
-      } catch (error) {
-        console.error(`An error occurred in Position ${position.id}: `, error);
-        logger.error(`An error occurred in Position ${position.id}: `, error);
-      }
-    };
-
     if (positions.length > 0) {
-      // await Promise.allSettled(positions?.map(liquidatePosition));
+      // await Promise.allSettled(
+      //   positions.map((position) =>
+      //     liquidatePosition(position, FlashLiquidate, accounts, helperContract)
+      //   )
+      // );
       for (let i = 0; i < positions.length; i++) {
-        await liquidatePosition(positions[i]);
+        await liquidatePosition(
+          positions[i],
+          FlashLiquidate,
+          accounts,
+          helperContract
+        );
       }
     }
   } catch (error) {
